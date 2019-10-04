13cc55d6a773331ac583fa00636be9b67f1953b9
Martin Hanzlik <hanzlik.martin.92@gmail.com>
2019-10-04 10:22:01 +0200

    move-right works

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,38 @@
+let xpos;
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('it works');
+  const pacboy = document.querySelector('.pacboy-active-light');
+
+  //saves the x-position of pacman on the screen
+  xpos = pacboy.offsetLeft;
+  
+  document.addEventListener('keydown', (event) =>{
+    
+      if(event.key === 'ArrowRight'){
+        console.log(pacboy.style.left);
+        pacboy.style.left = `${xpos + 85}px`;
+        xpos += 85;
+
+    }
+});
+
+
+  // open and close the pacman
+  document.addEventListener('keydown', (event) =>{
+    if(event.key === 'ArrowDown'){
+      
+      if (pacboy.style.backgroundPositionX !== '100%'){
+        
+        pacboy.style.backgroundPositionX = '100%';
+        
+      }
+      else{
+        pacboy.style.backgroundPositionX = '0%';
+        
+
+      }
+    }
+    
+  });
+  
 });
